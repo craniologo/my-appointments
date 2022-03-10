fc1b3fa306215ac448b2bf6084a13d1c21d33c01
Edgar Abanto <32318892+craniologo@users.noreply.github.com>
2022-03-09 21:10:06 -0500

    Add menu and schedule advances

--- app/Http/Controllers/Admin/PatientController.php
+++ app/Http/Controllers/Admin/PatientController.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\User;
+
+use App\Http\Controllers\Controller;
 
 class PatientController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,17 +8,21 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-//Specialty
-Route::get('/specialties', 'SpecialtyController@index');
-Route::get('/specialties/create', 'SpecialtyController@create'); // form register
-Route::get('/specialties/{specialty}/edit', 'SpecialtyController@edit');
+Route::middleware(['auth', 'admin'])->namespace('Admin')->group(function () {
+    //Specialty
+    Route::get('/specialties', 'SpecialtyController@index');
+    Route::get('/specialties/create', 'SpecialtyController@create'); // form register
+    Route::get('/specialties/{specialty}/edit', 'SpecialtyController@edit');
+    Route::post('/specialties', 'SpecialtyController@store'); // envío del form
+    Route::put('/specialties/{specialty}', 'SpecialtyController@update');
+    Route::delete('/specialties/{specialty}', 'SpecialtyController@destroy');
+    // Doctors
+    Route::resource('doctors', 'DoctorController');
+    // Patients
+    Route::resource('patients', 'PatientController');
+});
 
-Route::post('/specialties', 'SpecialtyController@store'); // envío del form
-Route::put('/specialties/{specialty}', 'SpecialtyController@update');
-Route::delete('/specialties/{specialty}', 'SpecialtyController@destroy');
-
-// Doctors
-Route::resource('doctors', 'DoctorController');
-
-// Patients
-Route::resource('patients', 'PatientController');
+Route::middleware(['auth', 'doctor'])->namespace('Doctor')->group(function () {
+    Route::get('/schedule', 'ScheduleController@edit');
+    Route::post('/schedule', 'ScheduleController@store');
+});
