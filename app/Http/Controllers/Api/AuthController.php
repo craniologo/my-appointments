3c9c5308758f7e07c91ef9f4997d7e5b9238faf3
Edgar Abanto <32318892+craniologo@users.noreply.github.com>
2022-09-09 06:42:59 -0500

    Apuntes retrofit

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -16,17 +16,23 @@ class AuthController extends Controller
         if (Auth::guard('api')->attempt($credentials)) {
             $user = Auth::guard('api')->user();            
             $jwt = JwtAuth::generateToken($user);
-            $error = false;
+            $success = true;
             
             // Return successfull sign in response with the generated jwt.
-            $data = compact('user', 'jwt');
-            return compact('error', 'data');
+            return compact('success', 'user', 'jwt');
         } else {
 
             // Return response for failed attempt...
-            $error = true;
+            $success = false;
             $message = 'Invalid credentials';
-            return compact('error', 'message');
+            return compact('success', 'message');
         }
+    }
+
+    public function logout()
+    {
+        Auth::guard('api')->logout();
+        $success = true;
+        return compact('success');
     }
 }
